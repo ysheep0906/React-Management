cca602bc917c92b36075127e9827122d0833955d
ysheep0906 <wlq94814@naver.com>
2023-12-06 17:42:35 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gray-background">
+        <img src={logo} alt="logo" />
+        <h2>Let;gasd masndsda asdd</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
