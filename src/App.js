37b526719c8c9aeb5a24aec5fb937120722a8e88
ysheep0906 <wlq94814@naver.com>
2023-12-06 20:59:42 +0900

    apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width:'100%',
+    marginTop: '30px',
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const cus = [{
   'id': 1,
@@ -26,14 +44,26 @@ const cus = [{
   'job': '대학생'
 }]
 
-function App() {
+function App(props) {
+  const classes = useStyles();
   return (
-    <div>
-      {
-        cus.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}></Customer>); })
-      }
-      
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableCell>번호</TableCell>
+          <TableCell>이미지</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>생년월일</TableCell>
+          <TableCell>성별</TableCell>
+          <TableCell>직업</TableCell>
+        </TableHead>
+        <TableBody>
+          {
+            cus.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}></Customer>); })
+          }
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,39 +1,23 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
+
 
 function Customer(props) {
   return (
     // JSX는 두 개 이상의 태그들이 나열되어있으면 무조건 div로 묶어야한다.
-    <div> 
-      <CustomerProfile
-      id={props.id}
-      image={props.image}
-      name={props.name}></CustomerProfile>
-      <CustomerInfo
-      birthday={props.birthday}
-      gender={props.gender}
-      job={props.job}></CustomerInfo>
-    </div>
+    <TableRow>
+      <TableCell>{props.id}</TableCell>
+      <TableCell><img src={props.image} alt="profile" /></TableCell>
+      <TableCell>{props.name}</TableCell>
+      <TableCell>{props.birthday}</TableCell>
+      <TableCell>{props.gender}</TableCell>
+      <TableCell>{props.job}</TableCell>
+    </TableRow>
   );
 }
 
-function CustomerProfile(props) {
-  return(
-    <div>
-      <img src={props.image} alt="profile" />
-      <h2>{props.name}({props.id})</h2>
-    </div>
-  );
-}
-
-function CustomerInfo(props) {
-  return (
-    <div>
-      <p>{props.birthday}</p>
-      <p>{props.gender}</p>
-      <p>{props.job}</p>
-    </div>
-  );
-}
 
 export default Customer;
