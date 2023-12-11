dc1cb8a56e0684d38961cf1007684e3b4e5f5034
ysheep0906 <wlq94814@naver.com>
2023-12-11 15:20:55 +0900

    Add Get Customers REST API

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import { makeStyles } from '@mui/styles';
+import axios from "axios";
+import { useState, useEffect } from 'react';
 
 const useStyles = makeStyles({
   root: {
@@ -19,32 +21,24 @@ const useStyles = makeStyles({
   }
 })
 
-const cus = [{
-  'id': 1,
-  'image': 'https://picsum.photos/id/10/64/64',
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://picsum.photos/id/20/64/64',
-  'name': '양신희',
-  'birthday': '001222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 3,
-  'image': 'https://picsum.photos/id/30/64/64',
-  'name': '권준석',
-  'birthday': '051222',
-  'gender': '남자',
-  'job': '대학생'
-}]
 
 function App(props) {
+
+  const [state, setState] = useState({
+    customers: ""
+  });
+
+  async function callApi() {
+    const res = await fetch('/api/customers');
+    const newState = { ...state };
+    newState.customers = await res.json();
+    setState(newState);
+  }
+
+  useEffect(() => {
+    callApi();
+  });
+
   const classes = useStyles();
   return (
     <Paper className={classes.root}>
@@ -59,7 +53,7 @@ function App(props) {
         </TableHead>
         <TableBody>
           {
-            cus.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}></Customer>); })
+            state.customers ? state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}></Customer>)}) : ""
           }
         </TableBody>
       </Table>
--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({message: 'Hello Express!'});
+
+app.get('/api/customers', (req,res) => {
+  res.send(
+    [{
+      'id': 1,
+      'image': 'https://picsum.photos/id/10/64/64',
+      'name': '홍길동',
+      'birthday': '961222',
+      'gender': '남자',
+      'job': '대학생'
+    },
+    {
+      'id': 2,
+      'image': 'https://picsum.photos/id/20/64/64',
+      'name': '양신희',
+      'birthday': '001222',
+      'gender': '남자',
+      'job': '대학생'
+    }, 
+    {
+      'id': 3,
+      'image': 'https://picsum.photos/id/30/64/64',
+      'name': '권준석',
+      'birthday': '051222',
+      'gender': '남자',
+      'job': '대학생'
+    }
+  ]);
 });
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
